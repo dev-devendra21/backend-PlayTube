7006d53bb1cdc8614d60b844a8ecd73abb5b52d4
Devendra Kumar <devendrakumaraa@gmail.com>
2024-05-07 12:39:18 +0530

    testing in postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
     const { fullName, username, email, password, } = req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     if (
         [fullName, username, email, password].some((field) => field?.trim() === "")
@@ -35,7 +35,13 @@ const registerUser = asyncHandler(async (req, res) => {
     if (existedUser) throw new ApiError(409, "User already exists")
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
 
 
     if (!avatarLocalPath) throw new ApiError(400, "Avatar is required")
